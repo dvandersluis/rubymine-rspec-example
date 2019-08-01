3acccf089d0c6bbd80be23985184ac62c78e7cb5
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2019-08-01 15:48:45 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+group :development, :test do
+  gem 'rspec', '~> 3.8.0'
+  gem 'pry'
+end
---
+++ spec/test_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+RSpec.describe do
+  it 'fails' do
+    arr = nil
+    expect(arr[:foo]).to eq(:bar)
+	end
+end
